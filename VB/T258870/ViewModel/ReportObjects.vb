d353550c1d20d3b64ee44a0f4742b55574d6e6b5
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 23:55:46 +0400

    Change example status to out of maintenance

--- VB/T258870/ViewModel/ReportObjects.vb
+++
@@ -1,9 +0,0 @@
-Namespace T258870.ViewModel
-    Public Class ReportObjects
-        Public Property DisplayName() As String
-        Public Property Id() As Integer
-        Public Overrides Function ToString() As String
-            Return DisplayName
-        End Function
-    End Class
-End Namespace
